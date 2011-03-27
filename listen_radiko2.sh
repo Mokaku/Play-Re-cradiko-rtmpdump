43f551abdca7af87e79f2285e1dcaee2a419ccf1
Hirofumi.Tobe <tobe@moka-mini9.(none)>
2011-03-27 17:13:19 +0900

    Add Mozzila User Agent to script

--- listen_radiko2.sh
+++ listen_radiko2.sh
@@ -57,6 +57,7 @@ wget -q \
      --post-data='\r\n' \
      --no-check-certificate \
      --save-headers \
+     --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)" \
      https://radiko.jp/v2/api/auth1_fms
 
 if [ $? -ne 0 ]; then
